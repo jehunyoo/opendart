e3081a8f008a8a4b3993c3ace55a304073150335
JehunYoo <percyu98@gmail.com>
2021-06-29 14:29:56 +0900

    [Feat] Add a function that makes a request url

---
+++ opendart/utils/url.py
@@ -0,0 +1,9 @@
+from opendart import config
+from opendart.config import BASE_URL
+
+
+def make_url(api_code, base=BASE_URL, **kwargs) -> str:
+    url = base + api_code + '?'
+    for key, value in kwargs.items():
+        url += f"{key}={value}&"
+    return url
