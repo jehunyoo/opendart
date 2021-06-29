183844035e8b0945ae307cb2b0f44df54b281923
JehunYoo <percyu98@gmail.com>
2021-06-29 14:25:14 +0900

    Add url configuration

---
+++ opendart/config/config.py
@@ -0,0 +1,10 @@
+"""
+Configuration for OpenDART
+"""
+
+# URLs
+BASE_URL = "https://opendart.fss.or.kr/api/"
+
+# Financial Information
+SINGLE_FS_XML = "fnlttSinglAcntAll.xml"
+SINGLE_FS_JSON = "fnlttSinglAcntAll.json"
